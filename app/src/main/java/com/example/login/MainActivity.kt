84e94a5014533c9f7ee1a9c2cad88ad0d66f6941
Subay22 <bayuadi00saputro@gmail.com>
2025-07-29 14:19:15 +0700

    kk

--- app/src/main/java/com/example/login/MainActivity.kt
+++ app/src/main/java/com/example/login/MainActivity.kt
@@ -42,6 +42,6 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 @Composable
 fun GreetingPreview() {
     LoginTheme {
-        Greeting("Android")
+        Greeting("Andromid")
     }
 }
